8131692ebe5938fa43b5151e16e8221c38d948ab
berksarikayaa <berksarikaaya@gmail.com>
2025-01-20 12:49:26 +0300

    Initial Commit

---
+++ UsedGamesTests/UsedGamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesTests.swift
+//  UsedGamesTests
+//
+//  Created by Buket Bayhan on 20.01.2025.
+//
+
+import Testing
+@testable import UsedGames
+
+struct UsedGamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
